ecfe18493e61b777d4ce155733039d0e194c5853
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2023-12-18 10:51:10 +0000

    [LeetCode Sync] Runtime - 139 ms (39.27%), Memory - 14.4 MB (6.81%)

---
+++ my-folder/2042-maximum-product-difference-between-two-pairs/solution.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def maxProductDifference(self, nums):
+        nums.sort()
+        for i in range(len(nums)):
+            s=(nums[-1]*nums[-2])-(nums[0]*nums[1])
+        return s
