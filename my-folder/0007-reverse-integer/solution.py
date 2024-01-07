e8ea93cd1cf8a86f7dbf7f29043475615ba18d39
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2024-01-07 13:25:02 +0000

    [LeetCode Sync] Runtime - 15 ms (75.33%), Memory - 13.3 MB (6.95%)

---
+++ my-folder/0007-reverse-integer/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def reverse(self, x):
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        # Reverse the digits
+        reversed_x = 0
+        while x != 0:
+            digit = x % 10
+            x //= 10
+
+            # Check for overflow before updating reversed_x
+            if reversed_x > (INT_MAX - digit) // 10:
+                return 0
+
+            reversed_x = reversed_x * 10 + digit
+
+        return sign * reversed_x
+
+
+        
