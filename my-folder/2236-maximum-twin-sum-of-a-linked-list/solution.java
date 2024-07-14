f75dbc3d6fd6d3a43c49d2dedb65509c00d6bbd8
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2024-07-14 05:26:58 +0000

    [LeetCode Sync] Runtime - 5 ms (63.59%), Memory - 63.5 MB (88.35%)

---
+++ my-folder/2236-maximum-twin-sum-of-a-linked-list/solution.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode Reverse(ListNode head){
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode Next = null;
+        while(curr!=null){
+            Next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = Next;
+        }
+        return prev;
+    }
+    public int pairSum(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = Reverse(slow.next);
+        int max = 0;
+        ListNode sum1 = head;
+        ListNode sum2 = slow.next;
+        while(sum2!= null){
+            int sum = sum1.val + sum2.val;
+            max = Math.max(max,sum);
+            sum1 = sum1.next;
+            sum2 = sum2.next;
+        }
+        return max;
+    }
+}
