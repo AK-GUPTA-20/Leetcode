4162930296410e547930e7ecc9b34999edb8b363
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2024-08-28 14:20:35 +0000

    [LeetCode Sync] Runtime - 1 ms (99.77%), Memory - 44.2 MB (93.12%)

--- my-folder/0036-valid-sudoku/solution.java
+++ my-folder/0036-valid-sudoku/solution.java
@@ -1,25 +1,25 @@
 class Solution {
-    public boolean isValid(char[][] board , int row , int col , int num){
-        // Checking row and column:-
-        for(int i=0 ; i<9 ; i++){
-            if(board[i][col] == num) return false;
-        }
-
+    private boolean check_isValid(int row , int col , char[][] board , int num){
+        // check the row
         for(int j=0 ; j<9 ; j++){
-            if(board[row][j] == num ) return false;
+            if(board[row][j]==num) return false;
         }
 
+        // check column
+        for(int i=0 ; i<9 ; i++){
+            if(board[i][col]==num) return false;
+        }
 
-        // checking in 3X3 Mstrix: 
-
-        int S_row = row/3*3;
-        int S_col = col/3*3;
-
-        for(int i=S_row ; i<S_row+3 ; i++){
-            for(int j=S_col ; j<S_col+3 ; j++){
-                if(board[i][j] == num) return false;
+        // check the 3X3 Matrix
+        int new_row = row/3*3;
+        int new_col = col/3*3;
+        for(int i=new_row ; i<new_row+3  ; i++){
+            for(int j=new_col ; j<new_col+3 ; j++){
+                if(board[i][j] == num ) return  false;
             }
         }
+
+
         return true;
     }
 
@@ -27,13 +27,14 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         for(int i=0 ; i<9 ; i++){
             for(int j=0 ; j<9 ; j++){
-                if(board[i][j] == '.')  continue;
+                if(board[i][j] == '.')    continue;
                 char num = board[i][j];
                 board[i][j] = '.';
-                if(isValid(board,i,j,num)==false) return false;
+                if(check_isValid(i,j,board,num)==false) return false;
                 board[i][j] = num;
             }
         }
+
         return true;
     }
 }
