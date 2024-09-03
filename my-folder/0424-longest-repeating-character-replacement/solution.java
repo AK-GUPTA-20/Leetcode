a6e1cafe3806bf0ebaec47968c72b8dd209c287c
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2024-09-03 04:34:31 +0000

    [LeetCode Sync] Runtime - 7 ms (87.63%), Memory - 42.6 MB (100.00%)

--- my-folder/0424-longest-repeating-character-replacement/solution.java
+++ my-folder/0424-longest-repeating-character-replacement/solution.java
@@ -1,29 +1,29 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int l = 0;
-        int r = 0;
-        int maxf = 0;
-        int maxlen = 0;
+        int l=0;
+        int r=0;
+        int maxf=0;
+        int maxlen=0;
         int[] hash = new int[26];
 
-        while (r < s.length()) {  
 
-            // Counting the number of characters
-            hash[s.charAt(r) - 'A']++;
-            maxf = Math.max(maxf, hash[s.charAt(r) - 'A']);
+        while(r<s.length()){
 
-            // Condition out of bound
-            while ((r - l + 1) - maxf > k) {
-                hash[s.charAt(l) - 'A']--;  
+            // finding maximum freq...
+            hash[s.charAt(r)-'A']++;
+            maxf = Math.max(maxf,hash[s.charAt(r) - 'A']);
+
+            // correct the invalid window by shrinking
+            // changes in string = ((r-l+1) - maxf)
+
+            while((r-l+1) - maxf > k){
+                hash[s.charAt(l)-'A']--;
                 l++;
             }
 
-            // Updating the maxlen
-            maxlen = Math.max(maxlen, r - l + 1);
-
+            maxlen = Math.max(maxlen,r-l+1);
             r++;
         }
         return maxlen;
     }
 }
-
