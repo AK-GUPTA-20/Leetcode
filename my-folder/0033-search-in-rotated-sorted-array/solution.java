3239ab9c8d338f48d6b02ed6590b460e352b738a
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2024-09-11 05:17:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.6 MB (44.70%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.java
+++ my-folder/0033-search-in-rotated-sorted-array/solution.java
@@ -1,41 +1,25 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int piv = piviot(nums);
-        int left = Binary_Searching(nums,target,0,piv);
-        int right = Binary_Searching(nums,target,piv+1,nums.length-1);
-        if(left != -1) return left;
-        else if(right != -1)  return right ;
-        else return -1;
-        
-    }
+        int low = 0;
+        int high = nums.length-1;
 
-
-    static int piviot(int[] arr ){
-        int max = Integer.MIN_VALUE;
-        int ind = 0;
-        for(int i=0 ; i<arr.length ; i++){
-            if (arr[i] > max){
-                max = arr[i];
-                ind = i;
-            }
-        }
-        return ind;
-
-    }    
-
-    static int Binary_Searching(int[] arr , int x , int l , int r) {
-        while(l<=r){
-            int mid = (l+r)/2;
-            if (arr[mid] == x){
-                return mid;
-            }
-            else if (x<arr[mid]) {
-                r=mid-1;
+        while(high>=low){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && target < nums[mid]){
+                    high = mid-1;
+                }
+                else low = mid+1;
             }
             else{
-                l=mid+1;
+                if(target > nums[mid] && target <= nums[high]){
+                    low = mid+1;
+                }
+                else high = mid-1;
             }
         }
+
         return -1;
     }
 }
