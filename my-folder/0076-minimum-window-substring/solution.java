7cee3c325caeffb588de14d684b1ebee08eb4118
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-07-28 13:15:38 +0000

    [LeetCode Sync] Runtime - 19 ms (50.42%), Memory - 45.4 MB (55.79%)

--- my-folder/0076-minimum-window-substring/solution.java
+++ my-folder/0076-minimum-window-substring/solution.java
@@ -1,44 +1,52 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (t.length() > s.length()) {
-            return "";
-        }
 
-        Map<Character, Integer> need = new HashMap<>();
+        // assign to t -> need
+        HashMap<Character, Integer> tMap = new HashMap<>();
         for (char ch : t.toCharArray()) {
-            need.put(ch, need.getOrDefault(ch, 0) + 1);
+            tMap.put(ch, tMap.getOrDefault(ch, 0) + 1);
         }
 
-        Map<Character, Integer> window = new HashMap<>();
-        int have = 0, needCount = need.size();
-        int left = 0, right = 0;
+        int left = 0;
+        int right = 0;
         int minLen = Integer.MAX_VALUE;
+        int have = 0;
+        int need = tMap.size();
         int start = 0;
 
+        HashMap<Character, Integer> window = new HashMap<>();
         while (right < s.length()) {
-            char ch = s.charAt(right);
-            window.put(ch, window.getOrDefault(ch, 0) + 1);
+            //assign to window -> need
+            char c = s.charAt(right);
+            window.put(c, window.getOrDefault(c, 0) + 1);
 
-            if (need.containsKey(ch) && window.get(ch).equals(need.get(ch))) {
+            if (tMap.containsKey(c) && window.get(c).equals(tMap.get(c))) {
                 have++;
             }
 
-            while (have == needCount) {
+            // window contract and find min length
+            while (have == need) {
                 if (right - left + 1 < minLen) {
                     minLen = right - left + 1;
                     start = left;
                 }
+                //safe delete(contarction) 
+                char leftCh = s.charAt(left);
+                window.put(leftCh, window.get(leftCh) - 1);
 
-                char leftChar = s.charAt(left);
-                window.put(leftChar, window.get(leftChar) - 1);
-                if (need.containsKey(leftChar) && window.get(leftChar) < need.get(leftChar)) {
+                if (tMap.containsKey(leftCh) && window.get(leftCh) < tMap.get(leftCh)) {
                     have--;
                 }
+
                 left++;
+
             }
+
             right++;
         }
+        if (minLen == Integer.MAX_VALUE)
+            return "";
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+        return s.substring(start, start + minLen);
     }
 }
