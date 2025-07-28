90c82790291f19bd6317ca597f4ebf12049285e8
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-07-28 17:25:09 +0000

    [LeetCode Sync] Runtime - 31 ms (27.04%), Memory - 44.6 MB (32.30%)

--- my-folder/0567-permutation-in-string/solution.java
+++ my-folder/0567-permutation-in-string/solution.java
@@ -1,44 +1,44 @@
-import java.util.HashMap;
-
-public class Solution {
+class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length())
+        if (s1.length()>s2.length()){
             return false;
-
-        HashMap<Character, Integer> s1Map = new HashMap<>();
-        HashMap<Character, Integer> windowMap = new HashMap<>();
-
-        
-        for (char c : s1.toCharArray()) {
-            s1Map.put(c, s1Map.getOrDefault(c, 0) + 1);
         }
 
-        
-        for (int i = 0; i < s1.length(); i++) {
-            char c = s2.charAt(i);
-            windowMap.put(c, windowMap.getOrDefault(c, 0) + 1);
+        HashMap<Character,Integer> s1count = new HashMap<>();
+        HashMap<Character,Integer> s2count = new HashMap<>();
+
+        for(int i=0 ; i<s1.length() ; i++){
+            char ch1 = s1.charAt(i);
+            char ch2 = s2.charAt(i);
+            s1count.put(ch1 , s1count.getOrDefault(ch1 ,0)+1);
+            s2count.put(ch2 , s2count.getOrDefault(ch2 ,0)+1);
         }
 
-        
-        if (s1Map.equals(windowMap)) return true;
+        if(s1count.equals(s2count)){
+            return true;
+        }
 
-        
-        for (int i = s1.length(); i < s2.length(); i++) {
-            char newChar = s2.charAt(i);                     
-            char oldChar = s2.charAt(i - s1.length());       
 
-            
-            windowMap.put(newChar, windowMap.getOrDefault(newChar, 0) + 1);
+        int left = 0 ;
+        int right = s1.length();
 
-            
-            if (windowMap.get(oldChar) == 1) {
-                windowMap.remove(oldChar);
-            } else {
-                windowMap.put(oldChar, windowMap.get(oldChar) - 1);
+        while(right<s2.length()){
+            char oldchar = s2.charAt(left);
+            if(s2count.get(oldchar)==1){
+                s2count.remove(oldchar);
             }
+            else{
+                s2count.put(oldchar , s2count.get(oldchar)-1);
+            }
+            left++;
 
-            
-            if (s1Map.equals(windowMap)) return true;
+            char newchar = s2.charAt(right);
+            s2count.put(newchar , s2count.getOrDefault(newchar ,0)+1);
+            right++;
+
+            if(s1count.equals(s2count)){
+            return true;
+            }
         }
 
         return false;
