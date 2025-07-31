0a6e22d90786d9810c95908f23191732c422a316
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-07-31 05:51:53 +0000

    [LeetCode Sync] Runtime - 1 ms (88.98%), Memory - 40.8 MB (91.35%)

--- my-folder/0007-reverse-integer/solution.java
+++ my-folder/0007-reverse-integer/solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public int reverse(int x) {
-        int res = 0;
-        boolean isNegative = x < 0;
-        String strX = String.valueOf(Math.abs(x));
-        StringBuilder sb = new StringBuilder(strX).reverse();
-        
-        try {
-            res = Integer.parseInt(sb.toString());
-        } catch (NumberFormatException e) {
-            return 0;
+        int rev_num = 0;
+        int last_digit = 0;
+        while(x!=0){
+             if (rev_num > Integer.MAX_VALUE / 10 || rev_num < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            last_digit = x%10;
+            rev_num = (rev_num * 10)+last_digit;
+            x=x/10;
         }
+        return rev_num;
         
-        return isNegative ? -res : res;       
     }
 }
