01f90c8210352d20114989b80db8303334a53d39
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-08-23 18:01:55 +0000

    [LeetCode Sync] Runtime - 15 ms (25.40%), Memory - 44.6 MB (49.63%)

--- my-folder/0242-valid-anagram/solution.java
+++ my-folder/0242-valid-anagram/solution.java
@@ -1,19 +1,30 @@
+import java.util.HashMap;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         }
+
+
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            if (!map.containsKey(c)) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+        }
+
         
-        int[] count = new int[26];
-        for(int i=0 ; i<s.length() ; i++){
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
+        for (int val : map.values()) {
+            if (val != 0) return false;
         }
-        
-        for(int val : count){
-            if(val!= 0) return false;
-        }
+
         return true;
-        
     }
 }
+
