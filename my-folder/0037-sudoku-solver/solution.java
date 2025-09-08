9112501469d284c911c9d04abaa325c4ea4a6722
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-09-08 12:25:54 +0000

    [LeetCode Sync] Runtime - 159 ms (76.64%), Memory - 41.1 MB (83.71%)

--- my-folder/0037-sudoku-solver/solution.java
+++ my-folder/0037-sudoku-solver/solution.java
@@ -1,12 +1,12 @@
 class Solution {
-    static int Check = 0;
+    static int check = 0;
 
-    public void solve(char[][] board, int row, int col) {
+    void solve(char[][] board, int row, int col) {
         if (row == 9) {
-            Check = 1;
+            check = 1;
             return;
         }
-        
+
         if (board[row][col] != '.') {
             if (col != 8) {
                 solve(board, row, col + 1);
@@ -14,42 +14,48 @@ class Solution {
                 solve(board, row + 1, 0);
             }
         } else {
-            for (char ch = '1'; ch <= '9'; ch++) {
-                if (isValid(board, row, col, ch)) {
-                    board[row][col] = ch;
+            for (char num = '1'; num <= '9'; num++) {
+                if (isValid(board, row, col, num)) {
+                    board[row][col] = num;
+
                     if (col != 8) {
                         solve(board, row, col + 1);
                     } else {
                         solve(board, row + 1, 0);
                     }
-                    if (Check == 1) return;
+
+                    if (check == 1) return;
+                    
                     board[row][col] = '.';
                 }
             }
         }
+
     }
 
-    public boolean isValid(char[][] board, int row, int col, char num) {
-        // Checking row and column
+    boolean isValid(char[][] board, int row, int col, char num) {
         for (int i = 0; i < 9; i++) {
-            if (board[i][col] == num || board[row][i] == num) return false;
-        }
-
-        // Checking in 3x3 Matrix
-        int S_row = row / 3 * 3;
-        int S_col = col / 3 * 3;
-
-        for (int i = S_row; i < S_row + 3; i++) {
-            for (int j = S_col; j < S_col + 3; j++) {
-                if (board[i][j] == num) return false;
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
             }
         }
+
+        int new_row = row / 3 * 3;
+        int new_col = col / 3 * 3;
+
+        for (int i = new_row; i < new_row + 3; i++) {
+            for (int j = new_col; j < new_col + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+
         return true;
     }
 
     public void solveSudoku(char[][] board) {
-        Check = 0; // Reset Check before solving
+        check = 0;
         solve(board, 0, 0);
     }
 }
-
