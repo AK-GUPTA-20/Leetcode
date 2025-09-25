dc624cb91f48a0d8e72b1c21f7edcc533604fbd0
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-09-25 16:27:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.6 MB (62.01%)

--- my-folder/2662-check-knight-tour-configuration/solution.java
+++ my-folder/2662-check-knight-tour-configuration/solution.java
@@ -1,47 +1,24 @@
 class Solution {
-    public boolean isSafe(int[][] grid , int row , int col , int num){
-        int n=grid.length;
-        int i,j;
-        
+    boolean checkValid(int[][] grid , int row , int col , int val){
+        int n = grid[0].length;
+        // base value check
+        if(row<0 || col<0 || row>=n || col>=n || grid[row][col] != val) return false;
+
         if(grid[row][col] == n*n-1) return true;
 
-        i=row-2;
-        j=col+1;
-        if(i<n && i>=0 && j<n && j>=0 && grid[i][j]==num+1) return isSafe(grid,i,j,num+1);
+        // check all the 8 configuration
+        boolean ans1 = checkValid(grid , row-1 , col+2 , val+1);
+        boolean ans2 = checkValid(grid , row-2 , col+1 , val+1);
+        boolean ans3 = checkValid(grid , row+1 , col+2 , val+1);
+        boolean ans4 = checkValid(grid , row+2 , col+1 , val+1);
+        boolean ans5 = checkValid(grid , row+2 , col-1 , val+1);
+        boolean ans6 = checkValid(grid , row+1 , col-2 , val+1);
+        boolean ans7 = checkValid(grid , row-1 , col-2 , val+1);
+        boolean ans8 = checkValid(grid , row-2 , col-1 , val+1);
 
-        i=row-2;
-        j=col-1;
-        if(i<n && i>=0 && j<n && j>=0 && grid[i][j]==num+1) return isSafe(grid,i,j,num+1);
-
-        i=row+2;
-        j=col+1;
-        if(i<n && i>=0 && j<n && j>=0 && grid[i][j]==num+1) return isSafe(grid,i,j,num+1);
-
-        i=row+2;
-        j=col-1;
-        if(i<n && i>=0 && j<n && j>=0 && grid[i][j]==num+1) return isSafe(grid,i,j,num+1);
-
-        i=row-1;
-        j=col+2;
-        if(i<n && i>=0 && j<n && j>=0 && grid[i][j]==num+1) return isSafe(grid,i,j,num+1);
-
-        i=row+1;
-        j=col+2;
-        if(i<n && i>=0 && j<n && j>=0 && grid[i][j]==num+1) return isSafe(grid,i,j,num+1);
-
-        i=row+1;
-        j=col-2;
-        if(i<n && i>=0 && j<n && j>=0 && grid[i][j]==num+1) return isSafe(grid,i,j,num+1);
-
-        i=row-1;
-        j=col-2;
-        if(i<n && i>=0 && j<n && j>=0 && grid[i][j]==num+1) return isSafe(grid,i,j,num+1);
-
-        return false;
+        return ans1 || ans2 || ans3 || ans4 || ans5 || ans6 || ans7 || ans8;
     }
-
     public boolean checkValidGrid(int[][] grid) {
-        if (grid[0][0] != 0) return false;
-        return isSafe(grid,0,0,0);
+        return checkValid(grid , 0 , 0 , 0 );
     }
 }
