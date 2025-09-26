1b1c7e9066f8f9633a4c6ce36914f47777b3a4d9
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-09-26 14:04:37 +0000

    [LeetCode Sync] Runtime - 24 ms (67.12%), Memory - 43.9 MB (35.04%)

---
+++ my-folder/0611-valid-triangle-number/solution.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = n - 1; i >= 2; i--) { 
+            int j = 0, k = i - 1;
+            while (j < k) {
+                if (nums[j] + nums[k] > nums[i]) {
+                    count += (k - j); 
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
+
