85a3e888f52b7eb698724379b96271021f388b78
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-10-17 10:33:46 +0000

    [LeetCode Sync] Runtime - 71 ms (45.24%), Memory - 58.1 MB (54.13%)

--- my-folder/0084-largest-rectangle-in-histogram/solution.java
+++ my-folder/0084-largest-rectangle-in-histogram/solution.java
@@ -1,48 +1,30 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        Stack<Integer> st = new Stack<>();
+        int maxArea = 0;
         int n = heights.length;
-        int[] nse = new int [n];     // next smallest element array
-        int[] pse = new int[n];      // prev smallest element array.
-
-
-        // claculating next smallest element array
-
-        nse[n-1] = n;
-        st.push(n-1);
-        for(int i=n-2 ; i>=0 ; i--){
-            while(!st.isEmpty() && heights[st.peek()] >= heights[i] ){
-                st.pop();
-
-
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i <= n; i++) {
+            // Use 0 height for the end of array to clear out remaining bars
+            int currHeight = (i == n) ? 0 : heights[i];
+            
+            while (!stack.isEmpty() && currHeight < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width;
+                
+                if (stack.isEmpty()) {
+                    width = i;
+                } else {
+                    width = i - stack.peek() - 1;
+                }
+                
+                maxArea = Math.max(maxArea, height * width);
             }
-            if(st.isEmpty()) nse[i] = n;
-            else nse[i] = st.peek();
-            st.push(i);
-
+            
+            stack.push(i);
         }
-
-        // clearing Stack
-        while(st.size()>0) st.pop();
-
-        // calculating prev smallest element (Index) array
-        pse[0] = -1;
-        st.push(0);
-        for(int k=1 ; k<=n-1 ; k++){
-            while(!st.isEmpty() && heights[st.peek()] >= heights[k]){
-                st.pop();
-            }
-            if(st.isEmpty()) pse[k] = -1;
-            else pse[k] = st.peek();
-            st.push(k);
-        }
-
-        // finding maximum area = heights[i] *(nse[i]-pse[i]-1)
-        int max = -1;
-        for(int j=0 ; j<n ; j++){
-            int area = heights[j] *(nse[j]-pse[j]-1);
-            max = Math.max(area,max);
-        }
-        return max;
+        
+        return maxArea;
     }
 }
+
