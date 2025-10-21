304772328e982a530d833cf7b416ad5e0629b3ea
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-10-21 07:54:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55.7 MB (98.54%)

--- my-folder/0882-peak-index-in-a-mountain-array/solution.java
+++ my-folder/0882-peak-index-in-a-mountain-array/solution.java
@@ -1,14 +1,13 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int max = arr[0];
-        int ind = -1;
-        for(int i=0 ; i<arr.length ; i++){
-            if (arr[i] > max){
-                max= arr[i];
-                ind = i;
-            }
+        int left = 0;
+        int right = arr.length-1;
+
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(arr[mid] > arr[mid+1]) right = mid-1;
+            else left = mid+1;
         }
-        return ind;
-        
+        return left;
     }
 }
