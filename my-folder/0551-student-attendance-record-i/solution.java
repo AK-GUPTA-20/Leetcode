2b86cbb1cf7a3e7770040a28dee6dc6b30ad82d9
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-11-28 10:14:11 +0000

    [LeetCode Sync] Runtime - 1 ms (27.38%), Memory - 43.2 MB (7.11%)

---
+++ my-folder/0551-student-attendance-record-i/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int absent=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='A')absent++;
+        }
+        if(absent<2 && !s.contains("LLL"))return true;
+        return false;
+    }
+}
