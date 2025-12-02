e5f86efa2d4b2b4c043c400970b5da8df70fff2d
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-12-02 05:13:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.5 MB (43.17%)

--- my-folder/0198-house-robber/solution.java
+++ my-folder/0198-house-robber/solution.java
@@ -1,20 +1,15 @@
+// optimal code 
 class Solution {
-    public int rob(int[] nums) {
-        int n = nums.length;
-
-        if (n == 1) {
-            return nums[0];
+    public int rob(int[] nums) {       
+        int secLast=nums[0];
+        if(nums.length<=1)return nums[0];
+        int last = Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            int curr = Math.max(last,nums[i]+secLast);
+            secLast=last;
+            last=curr;
         }
+        return last;
 
-        int[] dp = new int[n];
-
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
-        }
-
-        return dp[n - 1];        
-    }
+    } 
 }
